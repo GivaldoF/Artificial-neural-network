92c67056cb1a2b07183e1af002ba8ea45b1b8eb7
Givaldo Fernandes <42608150+GivaldoF@users.noreply.github.com>
2018-08-24 00:19:35 -0300

    Add files via upload

---
+++ Perceptron.py
@@ -0,0 +1,64 @@
+import numpy as np
+import random as rd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# Activation Function
+def f_at(x):
+    if x>= 0:
+        return 1
+    else:
+        return -1
+
+# Training set
+
+# Input set
+x = np.array([[-1, 0.1, 0.4, 0.7], [-1, 0.3, 0.7, 0.2], [-1, 0.6, 0.9, 0.8], [-1, 0.5, 0.7, 0.1]])
+
+# Result set
+d = np.array([1, -1, 1, 1])
+#x[0] ~ x[3] and x[1] ~ x[2]
+
+# bias
+bias = 0.5
+
+# Weight set
+w = np.array([bias, 0.1, 0.2, 0.3])
+
+# Learning constant
+eta = 0.1
+
+# Epochs
+epoca = 0
+erro = 1
+while erro == 1:
+    erro = 0
+    for i in range(len(x)):
+        u = np.sum(x[i] * w)
+        y = f_at(u)
+        if y != d[i]:
+            w = w + eta*(d[i] - y)*x[i]
+            erro = 1
+        epoca += 1
+
+print("Quantidade de epocas: ", epoca)
+print("Valores finais dos pesos:", w)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+normal = np.array([w[1], w[2], w[3]])
+
+xx,yy = np.meshgrid(range(2), range(2))
+
+z = (-w[1]*xx -w[2]*yy + w[0])/w[3]
+ax.plot_surface(xx, yy,z, color='white', alpha = 0.9)
+
+ax.scatter(x[0][1], x[0][2], x[0][3], color='green')
+ax.scatter(x[1][1], x[1][2], x[1][3], color='red')
+ax.scatter(x[2][1], x[2][2], x[2][3], color='green')
+ax.scatter(x[3][1], x[3][2], x[3][3], color='green')
+plt.show()
+
+
